f3834124d3566ae13c137bf75d9a71d34faea33e
ramsai24 <jnrsai24@gmail.com>
2023-12-06 21:00:34 +0530

    bt

---
+++ task4weekly08112023/jsFile.js
@@ -0,0 +1,47 @@
+// console.log("print");
+
+let generalTab = document.getElementById("nav-home-tab");
+let companyTab = document.getElementById("nav-profile-tab");
+let addressTab = document.getElementById("nav-contact-tab");
+let academicTab = document.getElementById("nav-academic-tab");
+let skillsTab = document.getElementById("nav-skills-tab");
+
+let general = document.getElementById("nav-home");
+let company = document.getElementById("nav-profile");
+let address = document.getElementById("nav-contact");
+let academic = document.getElementById("nav-academic");
+let skills = document.getElementById("nav-skills");
+
+// console.log(generalTab, companyTab, addressTab, academicTab, skillsTab);
+// console.log(general, company, address, academic, skills);
+let cancelGeneral = document.getElementById("cancelGeneral");
+let nextGeneral = document.getElementById("nextGeneral");
+
+// console.log(cancelGeneral, nextGeneral);
+
+let cancelCompany = document.getElementById("cancelCompany");
+let previousCompany = document.getElementById("previousCompany");
+let nextCompany = document.getElementById("nextCompany");
+
+// console.log(cancelCompany, previousCompany, nextCompany);
+
+let cancelAddress = document.getElementById("cancelAddress");
+let previousAddress = document.getElementById("previousAddress");
+let nextAddress = document.getElementById("nextAddress");
+
+// console.log(cancelAddress,previousAddress,nextAddress)
+
+let cancelAcademic = document.getElementById("cancelAcademic");
+let previousAcademic = document.getElementById("previousAcademic");
+let nextAcademic = document.getElementById("nextAcademic");
+
+// console.log(cancelAcademic, previousAcademic, nextAcademic);
+
+let cancelSkills = document.getElementById("cancelSkills");
+let previousSkills = document.getElementById("previousSkills");
+
+// console.log(cancelSkills, previousSkills);
+
+let e = document.getElementById("empty");
+
+e.value = general.vaue;
