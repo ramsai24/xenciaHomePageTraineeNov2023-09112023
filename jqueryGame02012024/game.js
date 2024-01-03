f7e0b0440430648fd99103bb254539ae22823840
ramsai24 <jnrsai24@gmail.com>
2024-01-03 18:32:01 +0530

    game task blink is working good but for repeated color not working

--- jqueryGame02012024/game.js
+++ jqueryGame02012024/game.js
@@ -1,6 +1,6 @@
 // $("#main").text("hi");
 
-$("#main").prepend("<h1>Level : <span id='spanEl'>3</span></h1>");
+$("#main").prepend("<h1>Level : <span id='spanEl'>5</span></h1>");
 $("#main").addClass("game-container");
 
 $("#one").append("<button id='red'>red</button>");
@@ -42,27 +42,85 @@ let obj = {};
 
 let ra;
 
+let userArray = [];
+
+$("#red").on("click", function () {
+  userArray.push($("#red").text());
+  console.log(userArray);
+});
+
+$("#green").on("click", function () {
+  userArray.push($("#green").text());
+  console.log(userArray);
+});
+
+$("#blue").on("click", function () {
+  userArray.push($("#blue").text());
+  console.log(userArray);
+});
+
+$("#yellow").on("click", function () {
+  userArray.push($("#yellow").text());
+  console.log(userArray);
+});
+
+// console.log(userArray);
+
 // console.log(randomColor);
 // console.log(countArray);
-
+let interval;
+let count = 0;
 $(document).ready(function () {
   $("h1").on("click", function () {
-    for (let i = 1; i <= levelValue; i++) {
-      console.log(i);
-      countArray.push(i);
-      // console.log(contentArray);
-      ra = shuffledEmojisList(contentArray);
-      // console.log(shuffledEmojisList(contentArray));
-      randomColor.push(ra[0]);
-      // console.log(countArray);
-      console.log(ra);
-      $(`#${ra[0]}`).animate({ opacity: 0.5 }, "slow");
-    }
-    console.log(randomColor);
-    console.log(countArray);
+    interval = setInterval(function () {
+      //   for (let i = 1; i <= levelValue; i++) {
+      count += 1;
+      if (count > levelValue) {
+        clearInterval(interval);
+      } else {
+        console.log(count);
+        countArray.push(count);
+        // console.log(contentArray);
+        ra = shuffledEmojisList(contentArray);
+        contentArray = ra;
+        // console.log(shuffledEmojisList(contentArray));
+        //   randomColor.push(ra[0]);
+        // console.log(countArray);
+        console.log(ra[0]);
+        // console.log(randomColor);
+        $(`#${ra[0]}`).addClass("opa");
+      }
+
+      //   setInterval(function () {
+      //     $(`#${ra[0]}`).addClass("opa");
+      //   }, 1000);
+      //   }
+    }, 2000);
+    $(`#${ra[0]}`).removeClass("opa");
+
+    // console.log(randomColor);
+    // console.log(countArray);
+    // console.log(userArray);
+    // console.log(randomColor);
+
+    // for (let e of randomColor) {
+    // setInterval(function () {
+    //   for (let e of randomColor) {
+    //     console.log(e);
+    //     $(`#${e}`).addClass("opa");
+    //   }
+    //   $;
+    // }, 5000);
+    // }
+
     //   for (let each of randomColor) {
     //     console.log(each);
     //     $(`#${each}`).animate({opacity:0.5})
     //   }
+    // if (userArray === randomColor) {
+    //   console.log(true);
+    //   console.log(userArray);
+    //   console.log(randomColor);
+    // }
   });
 });
